48f9fc7ba05a7c70aebb2daf92ac45c306c4f730
Mateusz Kowalski <mateusz.kowalski@swisscom.com>
2019-02-15 11:25:50 +0100

    Small refactor

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from __future__ import print_function
-from findradionuclideinventory import FindRadionuclideInventory
+from fma import FMA
+from scanner import scanner
 
+
+def FindRadionuclideInventory ():
+    originalInventory = []
+    scanner('Andra_FMA', originalInventory, '#')
+
+    # fake inventory
+    finv = [["H-3", float(9.84812486279e-06)],
+            ["Al-26", float(7.55041470047e-12)],
+            ["Ni-63", float(1.20418139837e-08)]]
+
+    FMA(finv)
+
+    return ()
 
 FindRadionuclideInventory()
